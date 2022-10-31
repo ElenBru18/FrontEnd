db3b2a2f3872c6ce17f7e427886c8fa25cdf997f
ElenBru18 <elenbruzzese@gmail.com>
2022-10-31 00:52:47 -0300

    modificaciones

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  firebase: {
+    projectId: 'frontendeyb',
+    appId: '1:27146853828:web:776b788a089a8cb89d5891',
+    storageBucket: 'frontendeyb.appspot.com',
+    locationId: 'southamerica-east1',
+    apiKey: 'AIzaSyDpit7lfIKf0U4ZyvXUepI6p4xTN8_TtGw',
+    authDomain: 'frontendeyb.firebaseapp.com',
+    messagingSenderId: '27146853828',
+  },
+  production: true,
+  URL: 'https://backendeyb.herokuapp.com/',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'frontendeyb',
+    appId: '1:27146853828:web:776b788a089a8cb89d5891',
+    storageBucket: 'frontendeyb.appspot.com',
+    locationId: 'southamerica-east1',
+    apiKey: 'AIzaSyDpit7lfIKf0U4ZyvXUepI6p4xTN8_TtGw',
+    authDomain: 'frontendeyb.firebaseapp.com',
+    messagingSenderId: '27146853828',
+  },
   production: false,
 
   URL: 'http://localhost:8080/', 
